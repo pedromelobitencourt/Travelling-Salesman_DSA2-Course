6fcc5935af6ad1cb9a187c59a1fa983951c2dde5
Pedro Vitor Melo Bitencourt <89912712+pedromelobitencourt@users.noreply.github.com>
2023-08-25 20:58:26 -0300

    Create Pair.java

---
+++ Pair.java
@@ -0,0 +1,17 @@
+public class Pair <T, P> {
+    private T first;
+    private P second;
+
+    public Pair(T first, P second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return this.first;
+    }
+
+    public P getSecond() {
+        return this.second;
+    }
+}
